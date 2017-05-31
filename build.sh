39e0eb42d998605dd73b27867f0db8b4e3f52ff4
fingul <fingul@gmail.com>
2017-05-31 17:21:11 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,8 +19,12 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
   --extra-ldflags="-L$HOME/ffmpeg_build/lib -static" \
   --extra-cflags="-I../nv_sdk" \
   --extra-ldflags="-L../nv_sdk" \
+  --enable-cuda \
+  --enable-cuvid \
+  --extra-cflags=-I/usr/local/cuda/include \
+  --extra-ldflags=-L/usr/local/cuda/lib64 \  
   --bindir="$HOME/bin" \
-  --enable-debug \
+  --disable-debug \
   --enable-gpl \
   --enable-libass \
   --enable-libfdk-aac \
